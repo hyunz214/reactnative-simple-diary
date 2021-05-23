8c8ae4087dfbf0678dcadf682b0f9223b2283ac2
HyeonJi Tak <jirae1997@naver.com>
2021-05-23 23:15:04 +0900

    rn 심플다이어리 초기 세팅

---
+++ pages/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+function List() {
+    <View>
+        <Text>
+            List
+        </Text>
+    </View>
+    
+}
+
+export default List;
---
+++ pages/View.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+function View() {
+    <View>
+        <Text>
+            View
+        </Text>
+    </View>
+}
+
+export default View;
